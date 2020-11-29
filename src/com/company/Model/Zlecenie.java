f22e784f2d8eb79c3fd51ed9660da239ac1372b5
marcin <marswider@gmail.com>
2020-11-29 20:45:17 +0100

    niedokończone modele.

--- src/com/company/Model/Zlecenie.java
+++ src/com/company/Model/Zlecenie.java
@@ -1,9 +1,9 @@
 package com.company.Model;
 
 public class Zlecenie {
-    int idZlecenia,
-    Zamowienie odpowiadajaceZamowienie,
-    Pracownik odpowiadajacyPracownik
+    int idZlecenia;
+    Zamowienie odpowiadajaceZamowienie;
+    Pracownik odpowiadajacyPracownik;
     public Zlecenie(
             int idZlecenia,
             Zamowienie odpowiadajaceZamowienie,
